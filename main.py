73fbcefb8f27c77d5e57c5ce40da23c54cc706e6
Alexander Marjin <amarjin6@gmail.com>
2022-05-22 16:38:35 +0300

    Create any path detection

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, url_for
 import os
 from dotenv import load_dotenv
+from werkzeug.routing import BaseConverter
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, map, *args):
+        self.map = map
+        self.regex = args[0]
 
 
 def configure():
@@ -10,11 +17,27 @@ def configure():
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('api_key')
 app.config['UPLOAD_FOLDER'] = 'static/downloads'
+app.url_map.converters['regex'] = RegexConverter
 
 
-@app.route('/', methods=['get'])
+@app.route('/home', methods=['get'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/<regex(".*"):path>', methods=['put'])
+def upload(path):
+    return path
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return '404 Not Found'
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return '400 Bad Request'
 
 
 if __name__ == '__main__':
