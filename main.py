0ee270401ed69505039d9079c268da6172abf63e
Alexander Marjin <amarjin6@gmail.com>
2022-05-22 15:37:49 +0300

    Create front for file storage

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, url_for
 import os
-from wtforms.validators import InputRequired
 from dotenv import load_dotenv
 
 
@@ -16,21 +12,9 @@ app.config['SECRET_KEY'] = os.getenv('api_key')
 app.config['UPLOAD_FOLDER'] = 'static/downloads'
 
 
-class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
-
-
-@app.route('/', methods=['GET', "POST"])
-@app.route('/home', methods=['GET', "POST"])
-def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data  # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
-                               secure_filename(file.filename)))  # Then save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+@app.route('/', methods=['get'])
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
